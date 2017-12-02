125cb3570f6bf6acb4d6360017c04d6df9e254e2
Morningxxx <spechles@spechles.com>
2017-12-02 22:48:07 +0800

    mv power line to last

--- vimsetup.sh
+++ vimsetup.sh
@@ -22,24 +22,6 @@ git clone https://github.com/danilo-augusto/vim-afterglow.git
 cp vim-afterglow/colors/afterglow.vim  ~/.vim/colors/
 rm -rf vim-afterglow
 
-# setup powerline
-pip install powerline-status
-
-. /Library/Python/2.7/site-packages/powerline/bindings/bash/powerline.sh
-
-echo << EOF >> ~/.bashrc
-. /Library/Python/2.7/site-packages/powerline/bindings/bash/powerline.sh
-EOF
-
-echo << EOF >> ~/.zshrc
-. /Library/Python/2.7/site-packages/powerline/bindings/bash/powerline.sh
-EOF
-
-git clone https://github.com/powerline/fonts.git
-cd fonts
-./install.sh
-rm -rf fonts
-
 # install plugin for vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
@@ -72,3 +54,22 @@ git clone https://github.com/Yggdroot/indentLine.git
 # jedi-vim
 # cd ~/.vim/bundle
 # git clone --recursive https://github.com/davidhalter/jedi-vim.git
+
+# setup powerline
+pip install powerline-status
+
+. /Library/Python/2.7/site-packages/powerline/bindings/bash/powerline.sh
+
+echo << EOF >> ~/.bashrc
+. /Library/Python/2.7/site-packages/powerline/bindings/bash/powerline.sh
+EOF
+
+echo << EOF >> ~/.zshrc
+. /Library/Python/2.7/site-packages/powerline/bindings/bash/powerline.sh
+EOF
+
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+rm -rf fonts
+
